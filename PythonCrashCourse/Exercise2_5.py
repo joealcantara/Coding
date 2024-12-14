17784ecf953c9906ba81bb973afa2b227e05294b
Joe Alcantara <joe@joealcantara.com>
2024-12-14 21:00:02 +0000

    added Exercise 2.5

---
+++ PythonCrashCourse/Exercise2_5.py
@@ -0,0 +1,6 @@
+# Exercise 2.5
+# Find a quote from a famous person you admire. Print the quote and the name of it's author.
+# Your output should look something like the following, including the quotation marks.
+# Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+print('James Barrat once said, "I don’t want to really scare you, but it was alarming how many people I talked to who are highly placed \npeople in AI who have retreats that are sort of ‘bug out’ houses, to which they could flee if it all hits the fan."')
