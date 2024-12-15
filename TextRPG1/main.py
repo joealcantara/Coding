f54f0631531986f3edaa079fa5bde2504897ca3a
Joe Alcantara <joe@joealcantara.com>
2024-12-15 21:45:42 +0000

    added new game

---
+++ TextRPG1/main.py
@@ -0,0 +1,30 @@
+# This is my first attempt at recreating and updating Deathtrap Dungeon.
+# Imports
+import random
+
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.maxStrength = random.randint(1, 6)
+        self.maxStamina = random.randint(2, 12)
+        self.maxLuck = random.randint(1, 6)
+        self.strength = self.maxStrength
+        self.stamina = self.maxStamina
+        self.luck = self.maxLuck
+
+def combat(player, monster):
+    pass
+
+def printCharacter(player):
+    print(player.name)
+    print(f"Strength: {player.strength}/{player.maxStrength}")
+    print(f"Stamina: {player.stamina}/{player.maxStamina}")
+    print(f"Luck: {player.luck}/{player.maxLuck}")
+
+def main():
+    print('Welcome to Text RPG')
+    print('')
+    player1 = player(input('What is your name? '))
+    printCharacter(player1)
+
+main()
